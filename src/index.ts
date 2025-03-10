75cbd4d2b041d1a5be0b54e178d946ef552a9d19
chizukicn <chizukicn@outlook.com>
2025-03-10 15:09:33 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
-import type { ConfigNames, OptionsConfig, Rules } from "@antfu/eslint-config";
+import type { ConfigNames, OptionsConfig, TypedFlatConfigItem } from "@antfu/eslint-config";
 import type { Linter } from "eslint";
 import type { Awaitable, FlatConfigComposer } from "eslint-flat-config-utils";
 import { antfu } from "@antfu/eslint-config";
 import defu from "defu";
 
 export type CurevOptions = OptionsConfig & Omit<TypedFlatConfigItem, "files">;
-
-export type TypedFlatConfigItem = Omit<Linter.Config<Linter.RulesRecord & Rules>, "plugins"> & {
-  /**
-   * An object containing a name-value mapping of plugin names to plugin objects. When `files` is specified, these plugins are only available to the matching files.
-   *
-   * @see [Using plugins in your configuration](https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new#using-plugins-in-your-configuration)
-   */
-  plugins?: Record<string, any>;
-};
 
 export type CurevUserConfig = Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>;
 
