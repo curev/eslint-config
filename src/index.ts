c941bcf8f6b77616e2d7ca96050e1a3b339bdda2
chizukicn <chizukicn@outlook.com>
2025-03-10 15:08:31 +0800

    feat: update @antfu/eslint-config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import type { OptionsConfig, TypedFlatConfigItem } from "@antfu/eslint-config";
+import type { ConfigNames, OptionsConfig, Rules } from "@antfu/eslint-config";
 import type { Linter } from "eslint";
 import type { Awaitable, FlatConfigComposer } from "eslint-flat-config-utils";
 import { antfu } from "@antfu/eslint-config";
 import defu from "defu";
 
 export type CurevOptions = OptionsConfig & Omit<TypedFlatConfigItem, "files">;
+
+export type TypedFlatConfigItem = Omit<Linter.Config<Linter.RulesRecord & Rules>, "plugins"> & {
+  /**
+   * An object containing a name-value mapping of plugin names to plugin objects. When `files` is specified, these plugins are only available to the matching files.
+   *
+   * @see [Using plugins in your configuration](https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new#using-plugins-in-your-configuration)
+   */
+  plugins?: Record<string, any>;
+};
+
+export type CurevUserConfig = Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>;
+
+export type CurevConfigNames = ConfigNames;
+
+export type CurevConfigs = FlatConfigComposer<TypedFlatConfigItem, CurevConfigNames>;
 
 export const defaultConfig: CurevOptions = {
   stylistic: {
@@ -27,7 +42,7 @@ export const defaultConfig: CurevOptions = {
   }
 };
 
-export function curev(options: CurevOptions = {}, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>[]) {
+export function curev(options: CurevOptions = {}, ...userConfigs: CurevUserConfig[]): CurevConfigs {
   return antfu(defu(
     options,
     defaultConfig,
