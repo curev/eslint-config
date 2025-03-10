60fdda1e6d9ea2b512629a3f4703e620586a0199
Chizuki <chizukicn@outlook.com>
2025-03-10 15:14:14 +0800

    feat: update @antfu/eslint-config (#8)
    * feat: update @antfu/eslint-config
    
    * chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import type { OptionsConfig, TypedFlatConfigItem } from "@antfu/eslint-config";
+import type { ConfigNames, OptionsConfig, TypedFlatConfigItem } from "@antfu/eslint-config";
 import type { Linter } from "eslint";
 import type { Awaitable, FlatConfigComposer } from "eslint-flat-config-utils";
 import { antfu } from "@antfu/eslint-config";
 import defu from "defu";
 
 export type CurevOptions = OptionsConfig & Omit<TypedFlatConfigItem, "files">;
+
+export type CurevUserConfig = Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>;
+
+export type CurevConfigNames = ConfigNames;
+
+export type CurevConfigs = FlatConfigComposer<TypedFlatConfigItem, CurevConfigNames>;
 
 export const defaultConfig: CurevOptions = {
   stylistic: {
@@ -27,7 +33,7 @@ export const defaultConfig: CurevOptions = {
   }
 };
 
-export function curev(options: CurevOptions = {}, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.Config[]>[]) {
+export function curev(options: CurevOptions = {}, ...userConfigs: CurevUserConfig[]): CurevConfigs {
   return antfu(defu(
     options,
     defaultConfig,
